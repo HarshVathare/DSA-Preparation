b8e2ba928d0ae7bc90fb557e7f852ae04981ce36
HarshVathare <harsh1234vathare@gmail.com>
2026-02-27 11:59:57 +0530

    fix bugs in merge sorted array

--- Array/Merge_Sorted_Elements.java
+++ Array/Merge_Sorted_Elements.java
@@ -18,18 +18,25 @@ public class Merge_Sorted_Elements {
     public static void MergeSortedElements(int[]nums1, int m, int[]nums2, int n) {
         int p1 = m-1;
         int p2 = n-1;
-        int ritePos = m+n-1;
+        int p = m+n-1;
 
-        while (p2 >= 0) {
-            if((p1 >= 0) && (nums1[p1] > nums2[p2])) {
-                ritePos = nums1[p1];
+        while (p1 >= 0 && p2 >= 0) {
+            if(nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
                 p1 -=1;
+                p-=1;
             }
             else {
-                ritePos = nums2[p2];
+                nums1[p] = nums2[p2];
                 p2 -=1;
+                p-=1;
             }
-            ritePos-=1;
+        }
+
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2 -=1;
+            p -=1;
         }
 
     }
